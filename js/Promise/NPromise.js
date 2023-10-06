e29a8b19a8422d7902cb3a43a4aa3f02941b2a44
LiuKun <13655914562@163.com>
2023-10-07 01:14:40 +0800

    完成Npromise的参数和resolve,rejected

--- js/Promise/NPromise.js
+++ js/Promise/NPromise.js
@@ -1,7 +1,49 @@
+/**
+ * pending fulfilled rejected 状态
+ */
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+/** 
+ * sdsdsdsdsd
+ * @param {Function} excutor 参数
+ */
 class NPromise {
-    id;
-    constructor(id) {
-        this.id = id;
+    constructor(excutor) {
+        this._state = PENDING;
+        this._value = undefined;
+        //不仅有resolve和rejected还有可能throw error
+        try {
+            excutor(this._resolve.bind(this), this._rejected.bind(this))
+            //_resolve和_rejected中都使用this，但是调用方式是resolve()直接调用，所以需要指定this
+        } catch (error) {
+            this._changeState(REJECTED, error)
+        }
     }
 
+    /**
+     * @param {String} newState 改变的NPrimise状态 
+     * @param {any} newValue  数据
+     */
+    _changeState(newState, newValue) {
+        //防止多次resolve或rejected throw,先判断是否第一次更改状态,只采用第一次
+        if (this._state !== PENDING) return
+        this._state = newState;
+        this._value = newValue;
+    }
+
+    /**
+     * @param {any} data 成功
+     */
+    _resolve(data) {
+        this._changeState(FULFILLED, data);
+    }
+
+    /**
+     * @param {any} reason 失败
+     */
+    _rejected(reason) {
+        this._changeState(REJECTED, reason)
+    }
 }
